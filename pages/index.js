2b6e5630e3ba2928ee6b3813ea577220a730d307
Rasmus Andersen <rasmus@Rasmus.lan>
2022-09-12 10:35:09 +0200

    fixed search filter

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,7 @@ const GetAll = gql`
         aar
         forBag
         type
+        typer
         kommentar
         varenummer {
           id
@@ -44,36 +45,46 @@ const GetAll = gql`
     modeller(orderBy: model_ASC) {
       model
     }
+    __type(name: "Typer") {
+      enumValues {
+        name
+      }
+    }
   }
 `;
 
 export async function getServerSideProps(context) {
-  const { maerker, modeller } = await graphcmsClient.request(GetAll)
+  const { maerker, modeller, __type } = await graphcmsClient.request(GetAll)
 
   return {
     props: {
       maerker,
       modeller,
+      __type,
     }
   }
 }
 
-export default function Home({ maerker, modeller }) {
-  const [loaded, setLoaded] = useState(false)
-  const [data, setData] = useState(maerker)
+export default function Home({ maerker, modeller, __type }) {
+  const [loaded, setLoaded] = useState(false);
+  const [data, setData] = useState(maerker);
   const [filtered, setFiltered] = useState(data);
-  const [models, setModels] = useState(modeller)
+  const [models, setModels] = useState(modeller);
   const [searchTerm, setSearchTerm] =useState("");
-  const [showOrder, setShowOrder] = useState(false)
+  const [showOrder, setShowOrder] = useState(false);
   const [cart, setCart] = useContext(CartContext);
-  const [dropdown, setDropdown] = useState(false)
-  const router = useRouter()
+  const [dropdown, setDropdown] = useState(false);
+  const [isTypesNull, setIsTypesNull] = useState(false);
+  const [type, setType] = useState("");
+  const router = useRouter();
+  const listRef = useRef(null);
+  const rowRef = useRef(null);
 
   const tilBestilling = () => {
     router.push('/bestil')
   }
 
-  const handleClick = (event, model) => {
+  const addToCart = (event, model) => {
     setCart(model)
     setShowOrder(true)
   }
@@ -86,7 +97,7 @@ export default function Home({ maerker, modeller }) {
     setLoaded(true)
   }, [])
 
-  console.log(data)
+  console.log(type)
 
   return (
     <>
@@ -138,12 +149,32 @@ export default function Home({ maerker, modeller }) {
                   <button key={tag.id} type='button' className={`${scss.tag} ${tag === filtered[0] && filtered != data ? `${scss.selected}` : ''}`} onClick={() => setFiltered([tag])}>
                     <span>
                       {tag.navn}
-                      <sup style={{ fontSize: '10px' }}>
-                        ({tag.modeller.length})
-                      </sup>
                     </span>
                   </button>
                 ))}
+              </div>
+            </div>
+            <div className={scss.filterContainer} >
+              <div className={scss.overskrift} onClick={handleDropdown}>
+                Filtrér efter type
+              </div>
+              <i className={scss.filterIcon}><FilterIcon /></i>
+              <div className={scss.openDropdown}>
+                <button className={
+                  `${scss.tag} ${scss.allTag} ${type === "" ? `${scss.selected}` : ''}`} onClick={() => setType("")}>
+                  <span>
+                    Alle
+                  </span>
+                </button>
+                { __type.enumValues.map((t, i) => (
+                  <button key={i} className={
+                  `${scss.tag} ${scss.allTag} ${type === t.name ? `${scss.selected}` : ''}`} onClick={() => setType(t.name)}>
+                    <span>
+                      {t.name}
+                    </span>
+                  </button>
+                ))}
+      
               </div>
             </div>
           </div>
@@ -152,32 +183,33 @@ export default function Home({ maerker, modeller }) {
           { loaded
             ? <div className={scss.listInner}>
               { filtered.filter((val) => {
-                if (searchTerm == "") {
+                if (searchTerm == "" && type == "") {
                   return val
-                } else if (
-                  val.modeller.some(y => y.model.toLowerCase().includes(searchTerm.toLowerCase()))
+                } else if (val.modeller.some(v =>
+                  (v.typer.toLowerCase() === type.toLowerCase() || type == "") && v.model.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
                 ) {
                   return val
                 }
               }).map(({navn, id, modeller}) => (
-                <div className={scss.models} key={id}>
+                <div className={scss.models} key={id} ref={listRef}>
                   <h2>
                     {navn}
                   </h2>
-                  <div className={scss.row}>
+                  <div className={scss.row} ref={rowRef}>
                     { modeller.filter((val) => {
-                      if (searchTerm == "") {
+                      if (searchTerm == "" && type == "") {
                         return val
                       } else if (
-                        val.model.toLowerCase().includes(searchTerm.toLowerCase())
+                        (val.typer.toLowerCase() == type.toLowerCase() || type == "") && val.model.toLowerCase().includes(searchTerm.toLowerCase())
                       ) {
                         return val
                       }
                     }).map((model) => (
                       <div
-                        className={scss.model}
+                        className={`${scss.model} model`}
                         key={model.id}
-                        onClick={event => handleClick(event, model)}
+                        onClick={event => addToCart(event, model)}
                         >
                         { model.model &&
                           <div className={scss.name}>
@@ -200,7 +232,7 @@ export default function Home({ maerker, modeller }) {
                 </div>
               ))}
             </div>
-            : <></>
+            : <><div>Indlæser modeller</div></>
           }
         </section>
       </section>
